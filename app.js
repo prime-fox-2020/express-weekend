26c21833e78c89af4015f9c1b106f27c449bddaf
fadhlanfm <fafadhlanriz@gmail.com>
2020-04-13 01:23:49 +0700

    data view on html table

--- app.js
+++ app.js
@@ -6,22 +6,34 @@ const port = 3000
 
 const teachers = fs.readFileSync('./data/teachers.json','utf8')
 const students = fs.readFileSync('./data/students.json','utf8')
-const subjects = fs.readFileSync('./data/teachers.json','utf8')
+const subjects = fs.readFileSync('./data/subjects.json','utf8')
+
+app.set('view engine', 'ejs')
+
+const message = ['Selamat datang di website SMP Negeri 1 Tangerang']
 
 app.get('/', function(req, res){
-    res.send('Selamat datang di website SMP Negeri 1 Tangerang')
+    res.render('home.ejs', {
+        data: message
+    })
 })
 
 app.get('/teachers', function(req, res){
-    res.send(JSON.parse(teachers))
+    res.render('teachers.ejs', {
+        data: JSON.parse(teachers)
+    })
 })
 
 app.get('/students', function(req, res){
-    res.send(JSON.parse(students))
+    res.render('students.ejs', {
+        data: JSON.parse(students)
+    })
 })
 
 app.get('/subjects', function(req, res){
-    res.send(JSON.parse(subjects))
+    res.render('subjects.ejs', {
+        data: JSON.parse(subjects)
+    })
 })
 
 
