10a0e85c543246f864555607c0928dd8f1c0e3d2
akmaliavitri <akmaliavitri21@gmail.com>
2020-04-12 08:04:31 +0700

    dengan router

--- app.js
+++ app.js
@@ -2,27 +2,36 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const students = require('./data/students.json')
-const teachers = require('./data/teachers.json')
-const subjects = require('./data/subjects.json')
-
-app.set('view engine', 'ejs')
-
 app.get('/', (req, res) => {
     res.send('halo')
 })
 
-app.get('/teachers', (req, res) => {
-    res.render('teachers.ejs', {teachers})
-})
+const students = require('./router/students')
+app.use(students)
 
-app.get('/students', (req, res) => {
-    res.render('students.ejs', {students})
-})
+const teachers = require('./router/teachers')
+app.use(teachers)
 
-app.get('/subjects', (req, res) => {
-    res.render('subjects.ejs', {subjects})
-})
+const subjects = require('./router/subjects')
+app.use(subjects)
+
+// const students = require('./data/students.json')
+// const teachers = require('./data/teachers.json')
+// const subjects = require('./data/subjects.json')
+
+// app.set('view engine', 'ejs')
+
+// app.get('/teachers', (req, res) => {
+//     res.render('teachers.ejs', {teachers})
+// })
+
+// app.get('/students', (req, res) => {
+//     res.render('students.ejs', {students})
+// })
+
+// app.get('/subjects', (req, res) => {
+//     res.render('subjects.ejs', {subjects})
+// })
 
 app.listen(port, (err, res) => {
     console.log('this app running on ports:', port)
---
+++ router/subjects.js
@@ -0,0 +1,16 @@
+const {Router} = require('express')
+const router = Router()
+
+const fs = require('fs')
+
+router.get('/subjects', (req, res) => {
+  fs.readFile('./data/subjects.json', 'utf8', (err, data) => {
+    if(err) {
+      res.send(err)
+    } else {
+      res.send(data)
+    }
+  })
+})
+
+module.exports = router
